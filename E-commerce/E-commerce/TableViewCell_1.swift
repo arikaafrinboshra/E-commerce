16abc6176ea2bb2d5db3067a6fc57469b18f37e2
Arika Afrin Boshra <arika@qcoom.com>
2021-05-24 18:25:55 +0600

    no message

--- E-commerce/E-commerce/TableViewCell_1.swift
+++ E-commerce/E-commerce/TableViewCell_1.swift
@@ -14,12 +14,105 @@ class TableViewCell_1: UITableViewCell {
     @IBOutlet weak var seeMoreBtn: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var productName_0 = ["Unstiched Embroidered",
+                         "Bioaqua Pink Cherry Lip Private",
+                         "STM Cap Style New Version Bike",
+                         "Polyester Half Fingered Hand",
+                         "Bio Oil Specialist Skincare Oil - 60ml",
+                         "New Women Tote Purse Leather",
+                         "Polyester Motorcycle Gloves",
+                         "Pro Biker Motorcycle Riding",
+                         "White Rice Whitening Pore",
+                         "Leather Backpack for Women -"]
+    
+    var productName_1 = ["BUY 1 Honda CB150R",
+                         "BUY 1 XSR 155cc Motor Bike and",
+                         "Buy Hoichoi Subscription - 12",
+                         "Buy Hoichoi Subscription - 12",
+                         "Buy 1 Bajaj Platina ES 100 Motor Bike",
+                         "Buy 1 Bajaj CT-100 ES Motor Bike and",
+                         "Buy 1 Bajaj Pulser 150cc Motor Bike",
+                         "Buy 1 Bajaj Pulser 150cc Double Disk",
+                         "Buy 1 Bajaj Discover 110cc",
+                         "Buy 1 Bajaj Discover 125CC"]
+    
+    var productName_2 = ["Long Beach Suites Dhaka - Long",
+                         "Long Beach Suites Dhaka - Executive",
+                         "Long Beach Suites Dhaka - Premier",
+                         "Long Beach Suites Dhaka - Premier",
+                         "Long Beach Suites Dhaka - Super",
+                         "Long Beach Suites Dhaka - Super",
+                         "Long Beach Suites Dhaka - Deluxe",
+                         "Ambrosia Guest House Deluxe Twin",
+                         "Ambrosia Guest House Deluxe",
+                         "Ambrosia Guest House Standard"]
+    
+    var productName_3 = ["Buy 1 OPPO F19 Smartphone -",
+                         "Buy 1 OPPO F19 Smartphone -",
+                         "Robi 10 Minute School 24 Hours",
+                         "Robi 10 Minute School English",
+                         "iCarnival Smart Gift Card - 10000 TK",
+                         "iCarnival Smart Gift Card - 5000 TK",
+                         "iCarnival Smart Gift Card - 2000 TK",
+                         "SSC Math Exam Preparation Course",
+                         "Onubhutir Obhidhan - Tahsan",
+                         "ghoori English Learning - BBC"]
+    
+    
+    
+    var productPrice_0 = ["৳ 1747",
+                          "৳ 249",
+                          "৳ 0",
+                          "৳ 332",
+                          "৳ 785",
+                          "৳ 1666",
+                          "৳ 322",
+                          "৳ 322",
+                          "৳ 304",
+                          "৳ 1632"]
+    
+    var productPrice_1 = ["৳ 550000",
+                          "৳ 525000",
+                          "৳ 700",
+                          "৳ 500",
+                          "৳ 99500",
+                          "৳ 95500",
+                          "৳ 169900",
+                          "৳ 180900",
+                          "৳ 117900",
+                          "৳ 131500"]
+    
+    var productPrice_2 = ["৳ 13920",
+                          "৳ 12528",
+                          "৳ 9744",
+                          "৳ 9048",
+                          "৳ 8352",
+                          "৳ 6960",
+                          "৳ 6264",
+                          "৳ 3360",
+                          "৳ 3360",
+                          "৳ 2800"]
+    
+    var productPrice_3 = ["৳ 21990",
+                          "৳ 21990",
+                          "৳ 399",
+                          "৳ 399",
+                          "৳ 7520",
+                          "৳ 3760",
+                          "৳ 1504",
+                          "৳ 960",
+                          "৳ 179",
+                          "৳ 115"]
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
-        seeMoreBtn.layer.cornerRadius = 5
+        //seeMoreBtn.layer.cornerRadius = 5
+        
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -28,5 +121,26 @@ class TableViewCell_1: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension TableViewCell_1: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return productName_0.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCollectionViewCell
+        
+        cell.name.text = productName_0[indexPath.row]
+        cell.price.text = productPrice_0[indexPath.row]
+        
+        return cell
+        
+    }
+    
 }
 
--- E-commerce/E-commerce/ViewController.swift
+++ E-commerce/E-commerce/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var searchBtn: UIButton!
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var homeTableView: UITableView!
+    
+    
+    var titleArr = ["Evaly Fashion Mall",
+                    "Evaly Foorti",
+                    "Hotel & Tours",
+                    "Mega Quick Deal"]
+    
+    var subTitleArr = ["For The Fashionable You!",
+                       "Buy 1 Get 1 Deals & More",
+                       "Best Hotels & Tour Deals",
+                       "Exclusive Offers!"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,20 +32,24 @@ class ViewController: UIViewController {
         homeTableView.dataSource = self
         searchBtn.layer.cornerRadius = 5
         
-//        self.tableView.rowHeight = UITableViewAutomaticDimension
-//        self.tableView.estimatedRowHeight = 200.0
+        
+        
+        //        self.tableView.rowHeight = UITableViewAutomaticDimension
+        //        self.tableView.estimatedRowHeight = 200.0
         
     }
-
-
+    
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        var c = titleArr.count
+        c+=1
         
-        return 1;
+        return c
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -41,13 +57,16 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         if (indexPath.row == 0) {
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell_0", for: indexPath) as! TableViewCell_0
-
+            
             return cell
         }
+        
         else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell_1", for: indexPath) as! TableViewCell_1
             
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell_0", for: indexPath) as! TableViewCell_1
-
+            cell.title.text = titleArr[indexPath.row - 1]
+            cell.subTitle.text = subTitleArr[indexPath.row - 1]
+            
             return cell
         }
         
